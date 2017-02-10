5d9649bdb14f57a9c433029fe6e175718f8f2b0b
seongminnpark <seongmi1@andrew.cmu.edu>
2017-02-10 13:28:27 -0500

    Add primitives

---
+++ gui/static/scripts/websocket.js
@@ -0,0 +1,44 @@
+// websocket.js
+
+$(document).ready(function() {
+
+    var socket = new WebSocket("ws://localhost:2441/websocket");
+
+    socket.onmessage = function (evt) {
+
+    	sensorData = JSON.parse(evt.data);
+
+        // console.log(sensorData);
+
+        var potMonitor = $('#potMonitor');
+        potMonitor.text(sensorData['Pot']);
+
+        var imuMonitor = $('#imuMonitor');
+        // ultMonitor.text(sensorData['Ult']);
+        imuData = sensorData['Imu'].split(";");
+        // console.log(imuData);
+        // console.log("%d, %d, %d", imuData[0],imuData[1], imuData[2]);
+        imuMonitor.text("X orientation: " + imuData[0].toString() +  "\n" + 
+                        ", Y orientation: " + imuData[1].toString() + "\n" + 
+                        ", Z orientation: " + imuData[2].toString());
+
+        var irMonitor = $('#irMonitor');
+        irMonitor.text(sensorData['Ir']);
+    };
+
+    socket.onclose = function() {
+
+    	var connectionClosedText= "Connection closed.";
+
+        var potMonitor = $('#potMonitor');
+        potMonitor.text(connectionClosedText);
+
+        var ultMonitor = $('#ultMonitor');
+        ultMonitor.text(connectionClosedText);
+
+        var irMonitor = $('#irMonitor');
+        irMonitor.text(connectionClosedText);
+    };
+});
+
+
